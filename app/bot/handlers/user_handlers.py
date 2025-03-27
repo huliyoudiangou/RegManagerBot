5687bbd3f6117876691d160cfc4e849a1d98dd68
senlief <zjanence@outlook.com>
2025-03-27 10:03:11 +0800

    add line

--- app/bot/handlers/user_handlers.py
+++ app/bot/handlers/user_handlers.py
@@ -28,7 +28,8 @@ def get_line_command(message):
         *音海拾贝 Navidrome 服务信息！*
         ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
         🌟服务器地址： （🈲禁网页播放，必ban）
-        https://music.makifx.com
+        https://music.makifx.com（直连x线路）
+        https://music2.makifx.com （CF线路）
 
         🌟歌词API地址 ： 
         https://lrc.makifx.com/lyrics（可用性未测）
